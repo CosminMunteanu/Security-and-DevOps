f6b7297637eac7a046a02d2ffa376d32cf20d036
Cosmin Munteanu <Cosmin.Munteanu@webfleet.com>
2022-09-22 13:43:52 +0200

    Implemented JWT authentication

---
+++ starter_code/src/main/java/com/example/demo/security/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.example.demo.security;
+
+public class SecurityConstants {
+    public static final long EXPIRATION_TIME = 100_000_000;
+    public static final String SECRET = "Cosmin's Secret";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer";
+    public static final String SIGNUP_URL = "/api/user/create";
+}
